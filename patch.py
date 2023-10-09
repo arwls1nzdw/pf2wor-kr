155e930705d1a7cb3b57ead5f45357ab221f7360
arwls1nzdw <arwls1nzdw@gmail.com>
2023-10-09 21:05:50 +0900

    replace link tag in kingmaker string to newer

--- patch.py
+++ patch.py
@@ -1,4 +1,6 @@
 import copy
+import json
+import re
 import shutil
 from glob import glob
 
@@ -29,14 +31,24 @@ data_en = utils.read_json("dist/enGB.json")
 km_en = utils.read_json("kingmaker/enGB.json")
 km_kr = utils.read_json("kingmaker/koKR.json")
 
+keys = {}
 count = 0
 for k, v in data_en['strings'].items():
-    if len(v) < 3:
+    if len(v) < 6:
         continue
 
-    if k in km_en and v[:3].lower() == km_en[k][:3].lower():
+    if k in km_en and v[:6].lower() == km_en[k][:6].lower():
         if km_kr[k] == data_kingmaker['strings'][k]:
             continue
+        r = r"<link=\"([a-zA-Z:_\-가-힣]+)\">"
+        km_kr[k] = km_kr[k].replace("</link>", "{/g}")
+        matches = re.findall(r, km_kr[k])
+        for m in matches:
+            km_kr[k] = km_kr[k].replace(f"<link=\"{m}\">", f"{{g|{m}}}")
+        if "<link=" in km_kr[k]:
+            print(k, km_kr[k])
+            raise Exception("link not replaced")
+        keys[k] = km_kr[k]
         data_kingmaker['strings'][k] = km_kr[k]
         data_namu['strings'][k] = km_kr[k]
         count += 1
@@ -52,3 +64,6 @@ utils.write_json(data_kingmaker, 'dist/koKR-km+namu.json')
 
 shutil.copyfile('dist/koKR.json', r'D:\Games\SteamLibrary\steamapps\common\Pathfinder Second Adventure\Wrath_Data\StreamingAssets\Localization\enGB.json')
 shutil.copyfile('dist/koKR-namu.json', r'D:\Games\SteamLibrary\steamapps\common\Pathfinder Second Adventure\Wrath_Data\StreamingAssets\Localization\zhCN.json')
+
+with open('keys.json', 'wt', encoding='utf-8') as f:
+    json.dump(keys, f, ensure_ascii=False, indent=4)
